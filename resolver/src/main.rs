3b2a2156dc09adb68b63860e496ec1335d8f82e6
Maxence Moutoussamy <maxence.moutoussamy@traefik.io>
2021-10-27 16:39:27 +0200

    add loop

--- resolver/src/main.rs
+++ resolver/src/main.rs
@@ -27,42 +27,42 @@ async fn main() -> Result<()> {
     let socket = UdpSocket::bind(&opt.bind_addr).await?;
     println!("Starting server on {}", opt.bind_addr);
 
-    // Reading socket.
-    let mut data = [0u8; 512];
-    let (_, src) = socket.recv_from(&mut data).await?;
+    loop {
+        // Reading socket.
+        let mut data = [0u8; 512];
+        let (_, src) = socket.recv_from(&mut data).await?;
 
-    // Parsing request data into DNS Packet.
-    let mut buffer = BytePacketBuffer::from_raw_data(&data);
-    let mut request = Packet::from_buffer(&mut buffer);
+        // Parsing request data into DNS Packet.
+        let mut buffer = BytePacketBuffer::from_raw_data(&data);
+        let mut request = Packet::from_buffer(&mut buffer);
 
-    // Creating response DNS Packet based on the request.
-    let mut response = Packet::new();
+        // Creating response DNS Packet based on the request.
+        let mut response = Packet::new();
 
-    // Taking the first question and resolve it. Maybe considering
-    // looping over all the questions in the future.
-    if let Some(question) = request.questions.pop() {
+        // Taking the first question and resolve it. Maybe considering
+        // looping over all the questions in the future.
+        if let Some(question) = request.questions.pop() {
 
-        // For now i'm only using the first root server but
-        // a better idea would be to randomly select the server
-        // from the root server list.
-        // let root_server = &ROOT_SERVERS[0];
-        // let server_ip = Ipv4Addr::from(root_server.1);
-        // response = recursive_lookup(&question.name, question.qtype, server_ip, opt.no_recursive)?;
+            // For now i'm only using the first root server but
+            // a better idea would be to randomly select the server
+            // from the root server list.
+            // let root_server = &ROOT_SERVERS[0];
+            // let server_ip = Ipv4Addr::from(root_server.1);
+            // response = recursive_lookup(&question.name, question.qtype, server_ip, opt.no_recursive)?;
 
-        response = Resolver::builder()
-            .recursive(!opt.no_recursive)
-            .build()
-            .resolve(question.name, question.qtype, request.header.recursion_desired)?;
+            response = Resolver::builder()
+                .recursive(!opt.no_recursive)
+                .build()
+                .resolve(question.name, question.qtype, request.header.recursion_desired)?;
+        }
+
+        response.header.id = request.header.id;
+        response.header.recursion_desired = request.header.recursion_desired;
+        response.header.recursion_available = !opt.no_recursive;
+        response.header.is_response = true;
+
+        let mut buffer = BytePacketBuffer::new();
+        response.serialize(&mut buffer)?;
+        socket.send_to(&buffer.bytes(), src).await?;
     }
-
-    response.header.id = request.header.id;
-    response.header.recursion_desired = request.header.recursion_desired;
-    response.header.recursion_available = !opt.no_recursive;
-    response.header.is_response = true;
-
-    let mut buffer = BytePacketBuffer::new();
-    response.serialize(&mut buffer)?;
-    socket.send_to(&buffer.bytes(), src).await?;
-
-    Ok(())
 }
